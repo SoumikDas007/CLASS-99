59830dc9483b1f4d75210b0122e94f6bc167d65d
SoumikDas007 <75881884+SoumikDas007@users.noreply.github.com>
2022-01-04 15:08:31 +0530

    Add files via upload

---
+++ moving.py
@@ -0,0 +1,7 @@
+import os
+#os.system("time")
+os.mkdir("D:/SUBHAM/WHITEHAT/PYTHON/newfolder1")
+os.getcwd()
+path="D:/SUBHAM/WHITEHAT/PYTHON/CLASS 99/newfolder"
+isExist= os.path.exists(path)
+print(isExist)
